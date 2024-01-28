4d68813bb6d4c8651940108e04f01a4324b40158
Ruslan Senkin <rsenkin24@gmail.com>
2024-01-28 11:55:38 +0200

    main cleaning

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ import Model.ExchangeOperation;
 
 public class Main {
     public static void main(String[] args) {
-        ExchangeOperation exchangeOperation =new ExchangeOperation(
-               Currency.EUR,Currency.USD,1000.22342432        );
-        System.out.println(exchangeOperation);
+
 
     }
 }
