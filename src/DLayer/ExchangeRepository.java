442486e5bddd2b8f0a7f608fb87ec188596b5874
Andrej Rusetski <rustven@gmail.com>
2024-01-28 12:32:19 +0100

    docs: add file with classxchangeRepository and empty methods

---
+++ src/DLayer/ExchangeRepository.java
@@ -0,0 +1,23 @@
+package DLayer;
+
+import Model.ExchangeOperation;
+
+import java.util.ArrayList;
+
+public class ExchangeRepository {
+
+    public boolean addOperation(ExchangeOperation operation){
+        return false;
+    }
+
+    public ExchangeOperation getOperation(int id){
+        return null;
+    }
+
+    public ArrayList<ExchangeOperation> getAllOpertions(){
+        return null;
+    }
+
+
+
+}
