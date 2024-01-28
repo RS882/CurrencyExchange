7b066fd55a582a4a0ea192e9c695afcd897cf725
Mihail Romanciuc <roman.mihail@yahoo.com>
2024-01-28 13:12:18 +0100

    docs: add file with class ExchangeService and empty methods

---
+++ src/ServiceLayer/ExchangeService.java
@@ -0,0 +1,20 @@
+package ServiceLayer;
+
+import Model.ExchangeOperation;
+
+import java.util.ArrayList;
+
+public class ExchangeService {
+    public double exchange(ExchangeOperation exchangeOperation) {
+        return 0;
+    }
+
+    public ExchangeOperation getOperation(int id) {
+        return null;
+    }
+
+    public ArrayList<ExchangeOperation> getAllOpertions() {
+        return null;
+    }
+
+}
