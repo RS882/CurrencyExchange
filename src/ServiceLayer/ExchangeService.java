5c4781d50fbc6fc0be37f1608c354776457aec98
Mihail Romanciuc <roman.mihail@yahoo.com>
2024-01-30 13:41:46 +0100

    ExchangeService

--- src/ServiceLayer/ExchangeService.java
+++ src/ServiceLayer/ExchangeService.java
@@ -1,20 +1,55 @@
 package ServiceLayer;
 
+import Currency.Currency;
+import DLayer.ExchangeRepository;
 import Model.ExchangeOperation;
 
 import java.util.ArrayList;
 
 public class ExchangeService {
-    public double exchange(ExchangeOperation exchangeOperation) {
-        return 0;
+
+    // Private field 'repository' of type ExchangeRepository,
+    // which will be used for interacting with exchange operation data:
+    private ExchangeRepository repository;
+
+    public ExchangeService(ExchangeRepository repository) {
+        this.repository = repository;
     }
 
+
+    // Public method that performs currency exchange based on the provided parameters of amount and currencies.
+    // Creates a new exchange operation based on the provided parameters.
+    // Calls the helper method calculateExchange to compute the exchange amount.
+    // Sets the calculated exchange amount in the created operation.
+    // repository.addOperation(operation): Adds the operation to the repository for saving the history.
+
+    public double exchange(double sumIn, Currency currencyIn, Currency currencyOut) {
+        ExchangeOperation operation = new ExchangeOperation(currencyIn, currencyOut, sumIn);
+        double sumOut = calculateExchange(operation);
+        operation.setSumOut(sumOut);
+        repository.addOperation(operation);
+        return sumOut;
+    }
+    // Logic for calculating the exchange amount, taking into account the exchange rate and commission:
+    private double calculateExchange(ExchangeOperation operation) {
+        double exchangeRate = operation.getRate(operation.getCurrencyIn(), operation.getCurrencyOut());
+        double sumOut = calculatedSumOut(operation, exchangeRate);
+        return sumOut;
+    }
+
+// Additional methods, if necessary
+    private double calculatedSumOut(ExchangeOperation operation, double exchangeRate) {
+        double sumOut = operation.getSumIn() * exchangeRate * (1 - operation.getCOMMISION());
+        return sumOut;
+    }
+    // Public method that returns the exchange operation for the given identifier:
     public ExchangeOperation getOperation(int id) {
+        // Implement this method to retrieve operation by id from the repository
         return null;
     }
 
-    public ArrayList<ExchangeOperation> getAllOpertions() {
+    public ArrayList<ExchangeOperation> getAllOperations() {
+        // Implement this method to retrieve all operations from the repository
         return null;
     }
-
 }
--- src/UILayer/ExchangeUI.java
+++ src/UILayer/ExchangeUI.java
@@ -1,50 +1,15 @@
 package UILayer;
-
-import Currency.Currency;
-import DLayer.ExchangeRepository;
-import Model.ExchangeOperation;
-
 public class ExchangeUI {
+    public static void run() {
 
-    //Приватное поле repository типа ExchangeRepository,
-// которое будет использоваться для взаимодействия с данными об операциях обмена:
-    private ExchangeRepository repository;
+    }
+    public static void exchange() {
 
-    // Конструктор:
-    public  ExchangeService (ExchangeRepository repository) {
-        this.repository = repository;
     }
-    // Публичный метод, выполняющий обмен валюты на основе переданных параметров суммы и валют.
-    // Создание новой операции обмена валюты на основе переданных параметров.
-    // Вызов вспомогательного метода calculateExchange для вычисления суммы для обмена.
-    //Установка рассчитанной суммы обмена в созданной операции.
-    //repository.addOperation(operation): Добавление операции в репозиторий для сохранения истории:
+    public static void getOperation() {
 
-    public double exchange(double sumIn, Currency currencyIn, Currency currencyOut) {
-        ExchangeOperation operation = new ExchangeOperation(currencyIn, currencyOut, sumIn);
-        double sumOut = calculateExchange(operation);
-        operation.setSumOut(sumOut);
-        // Сохранение операции в репозитории
-        repository.addOperation(operation);
-        return sumOut;
     }
-    // Логика вычисления суммы для обмена, учета курса обмена и комиссии:
-    private double calculateExchange(ExchangeOperation operation) {
-        return calculatedSumOut;
-    }
-    // Публичный метод, возвращающий операцию обмена по заданному идентификатору:
-    public ExchangeOperation getOperation(int id) {
-        return repository.getOperation(id);
-    }
-    // Публичный метод, врзвращающий метод всех операций:
-    public List getAllOperations() {
-        return repository.getAllOperations(); }
+    public static void getAllOperations() {
 
-    // Дополнительные методы, если необходимо
-    private double calculatedSumOut (ExchangeOperation operation){
-        double exchangeRate = operation.getRate(operation.getCurrencyIn(), operation.getCurrencyOut());
-        double sumOut = operation.getSumIn() * exchangeRate * (1 - operation.getCOMMISION()){
-            return sumOut;
-        }
     }
 }
