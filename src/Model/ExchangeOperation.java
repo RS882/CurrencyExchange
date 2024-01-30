b64c355fbcc6cdba61c25de05f69e0498b0674a0
Ruslan Senkin <rsenkin24@gmail.com>
2024-01-30 18:31:38 +0200

    refactoring

--- src/Model/ExchangeOperation.java
+++ src/Model/ExchangeOperation.java
@@ -75,7 +75,7 @@ public class ExchangeOperation {
         return this.COMMISION;
     }
 
-    public double getRate() {
+    public double getRate(Currency currencyIn, Currency currencyOut) {
         return this.rate;
     }
 
