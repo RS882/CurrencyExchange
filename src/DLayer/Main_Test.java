032aca84dc82763c695d60a2375ed0627708e903
Andrej Rusetski <rustven@gmail.com>
2024-01-30 20:14:20 +0100

    add new filds to ExchangeRepository class

---
+++ src/DLayer/Main_Test.java
@@ -0,0 +1,12 @@
+package DLayer;
+
+import Currency.Currency;
+import Model.ExchangeOperation;
+
+public class Main_Test {
+    public static void main(String[] args) {
+        ExchangeRepository exchangeRepository = new ExchangeRepository();
+      //  exchangeRepository.writeNewLine(new ExchangeOperation(Currency.EUR,Currency.CHF,1.9));
+        exchangeRepository.addOperation(new ExchangeOperation(Currency.EUR,Currency.CHF,2.9));
+    }
+}
